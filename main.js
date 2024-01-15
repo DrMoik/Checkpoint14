1c4d826dc0142955fbaded8b98cd0f3813bb1b1e
Miguel GZ <m.galvez@tec.mx>
2024-01-15 04:03:40 +0000

    Joystick running

---
+++ main.js
@@ -0,0 +1,138 @@
+var app = new Vue({
+    el: '#app',
+    // storing the state of the page
+    data: {
+        connected: false,
+        ros: null,
+        logs: [],
+        loading: false,
+        rosbridge_address: 'wss://i-0a64760e819256b63.robotigniteacademy.com/95d53c82-a09d-450e-ba0c-3dccaad056c5/rosbridge/',
+        port: '7000',
+        // dragging data
+        dragging: false,
+        x: 'no',
+        y: 'no',
+        dragCircleStyle: {
+            margin: '0px',
+            top: '0px',
+            left: '0px',
+            display: 'none',
+            width: '75px',
+            height: '75px',
+        },
+        // joystick valules
+        joystick: {
+            vertical: 0,
+            horizontal: 0,
+        },
+        // publisher
+        pubInterval: null,
+
+    },
+    // helper methods to connect to ROS
+    methods: {
+        connect: function() {
+            this.loading = true
+            this.ros = new ROSLIB.Ros({
+                url: this.rosbridge_address
+            })
+            this.ros.on('connection', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Connected!')
+                this.connected = true
+                this.loading = false
+            })
+            this.ros.on('error', (error) => {
+                this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
+            })
+            this.ros.on('close', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
+                this.connected = false
+                this.loading = false
+            })
+        },
+        publish: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: this.joystick.vertical, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: this.joystick.horizontal, },
+            })
+            topic.publish(message)
+        },
+
+        disconnect: function() {
+            this.ros.close()
+        },
+
+        sendCommand: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: this.joystick.vertical, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: this.joystick.horizontal },
+            })
+            topic.publish(message)
+        },
+        startDrag() {
+            this.dragging = true;
+            let ref = document.getElementById('dragstartzone');
+            let circleCenterX = ref.offsetLeft + ref.offsetWidth / 2;
+            let circleCenterY = ref.offsetTop + ref.offsetHeight / 2;
+            this.x = circleCenterX;
+            this.y = circleCenterY;
+            this.dragCircleStyle.display = 'inline-block';
+            this.dragCircleStyle.left = `${this.x - parseInt(this.dragCircleStyle.width) / 2}px`;
+            this.dragCircleStyle.top = `${this.y - parseInt(this.dragCircleStyle.height) / 2}px`;
+        },
+        stopDrag() {
+            this.dragging = false
+            this.x = this.y = 'no'
+            this.dragCircleStyle.display = 'none'
+            this.resetJoystickVals()
+            this.publish();
+        },
+        doDrag(event) {
+            if (this.dragging) {
+                this.x = event.offsetX
+                this.y = event.offsetY
+                let ref = document.getElementById('dragstartzone')
+                this.dragCircleStyle.display = 'inline-block'
+
+                let minTop = ref.offsetTop - parseInt(this.dragCircleStyle.height) / 2
+                let maxTop = minTop + 200
+                let top = this.y + minTop
+                this.dragCircleStyle.top = `${top}px`
+
+                let minLeft = ref.offsetLeft - parseInt(this.dragCircleStyle.width) / 2
+                let maxLeft = minLeft + 200
+                let left = this.x + minLeft
+                this.dragCircleStyle.left = `${left}px`
+
+                this.setJoystickVals()
+                this.publish();
+            }
+        },
+        setJoystickVals() {
+            this.joystick.vertical = -0.4 * ((this.y / 200) - 0.5)
+            this.joystick.horizontal = -1.0 * ((this.x / 200) - 0.5)
+        },
+        resetJoystickVals() {
+            this.joystick.vertical = 0
+            this.joystick.horizontal = 0
+        },
+
+
+
+
+    },
+    mounted() {
+
+    window.addEventListener('mouseup', this.stopDrag)
+    },
+})
