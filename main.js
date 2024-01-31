e921dcaa8f5aabbc40e0343ba3468819ffd11b0b
Miguel GZ <m.galvez@tec.mx>
2024-01-31 00:13:47 +0000

    3D viewer: Not finished

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var app = new Vue({
         ros: null,
         logs: [],
         loading: false,
-        rosbridge_address: 'wss://i-0a64760e819256b63.robotigniteacademy.com/95d53c82-a09d-450e-ba0c-3dccaad056c5/rosbridge/',
+        rosbridge_address: 'wss://i-0caa922cecd1aae95.robotigniteacademy.com/5bf9f262-b781-430d-b435-3ab1e03dd957/rosbridge/',
         port: '7000',
         // dragging data
         dragging: false,
@@ -27,6 +27,10 @@ var app = new Vue({
         },
         // publisher
         pubInterval: null,
+        // 3D stuff
+        viewer: null,
+        tfClient: null,
+        urdfClient: null,
 
     },
     // helper methods to connect to ROS
@@ -65,6 +69,48 @@ var app = new Vue({
 
         disconnect: function() {
             this.ros.close()
+        },
+
+        setup3DViewer() {
+            this.viewer = new ROS3D.Viewer({
+                background: '#cccccc',
+                divID: 'div3DViewer',
+                width: 40,
+                height: 30,
+                antialias: true,
+                fixedFrame: 'odom'
+            })
+
+            // Add a grid.
+            this.viewer.addObject(new ROS3D.Grid({
+                color:'#0181c4',
+                cellSize: 0.5,
+                num_cells: 20
+            }))
+
+            // Setup a client to listen to TFs.
+            this.tfClient = new ROSLIB.TFClient({
+                ros: this.ros,
+                angularThres: 0.01,
+                transThres: 0.01,
+                rate: 10.0
+            })
+
+            // Setup the URDF client.
+            this.urdfClient = new ROS3D.UrdfClient({
+                ros: this.ros,
+                param: '/robot_description',
+                tfClient: this.tfClient,
+                // We use "path: location.origin + location.pathname"
+                // instead of "path: window.location.href" to remove query params,
+                // otherwise the assets fail to load
+                path: location.origin + location.pathname,
+                rootObject: this.viewer.scene,
+                loader: ROS3D.COLLADA_LOADER_2
+            })
+        },
+        unset3DViewer() {
+            document.getElementById('div3DViewer').innerHTML = ''
         },
 
         sendCommand: function() {
